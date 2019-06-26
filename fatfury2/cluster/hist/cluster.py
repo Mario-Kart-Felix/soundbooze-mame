26ee06942c99e77e39c72cd66436ecae3424628d
soundbooze <soundbooze@gmail.com>
2019-06-26 22:58:52 +0700

    pre

--- fatfury2/cluster/hist/cluster.py
+++ fatfury2/cluster/hist/cluster.py
@@ -13,12 +13,15 @@ def create_train_kmeans(data, number_of_clusters):
 
 h = sys.argv[1]
 H = numpy.load(h)
+I = numpy.zeros(len(H))
+Z = numpy.array(zip(I, H))
 
-k = create_train_kmeans(H, int(sys.argv[2]))
+cluster = int(sys.argv[2])
+k = create_train_kmeans(Z, cluster)
 
 Z = []
 for i in H:
-    p = k.predict([i])
+    p = k.predict([[0, i]])[0]
     Z.append(p)
 
 plt.plot(Z)
