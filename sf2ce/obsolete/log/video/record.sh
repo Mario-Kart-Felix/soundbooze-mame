383431d03745bbeb2f142160b8e63b33c8067a14
soundbooze <soundbooze@gmail.com>
2019-06-29 19:38:04 +0700

    pre

--- sf2ce/obsolete/log/video/record.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-DATE=`date +"%Y-%m-%d-%H-%M-%S"`
-ffmpeg -video_size 541x406 -framerate 25 -f x11grab -i :0.0+107,114 $DATE".mp4"
--- sf2ce/obsolete/log/video/trim-video.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-ffmpeg -i $1 -ss 00:00:$3 -t 00:00:$4 -async 1 $2
-
-# ./trim-video.sh 1.mkv out.mkv 04 10
