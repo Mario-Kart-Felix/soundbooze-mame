2a33d99a4d06422c8e2e58a7049804524b1f1177
soundbooze <soundbooze@gmail.com>
2019-06-30 12:02:30 +0700

    pre

--- log/key/logger.py
+++ log/key/logger.py
@@ -1,11 +1,28 @@
 import time
 from pynput.keyboard import Key, Listener
 
+attr = ['char', 'up', 'down', 'left', 'right']
+
 def on_press(key):
-    print('{0}'.format(key))
+
+    if hasattr(key, attr[0]):
+        print time.time(), 0, key.char
+
+    elif hasattr(key, attr[1]) or hasattr(key, attr[2]) or hasattr(key, attr[3]) or hasattr(key, attr[4]):
+        k = ('{0}'.format(key))
+        z = k.split('.')
+        print time.time(), 0, z[1]
 
 def on_release(key):
-    print('{0} off'.format(key))
+
+    if hasattr(key, attr[0]):
+        print time.time(), 1, key.char
+
+    elif hasattr(key, attr[1]) or hasattr(key, attr[2]) or hasattr(key, attr[3]) or hasattr(key, attr[4]):
+        k = ('{0}'.format(key))
+        z = k.split('.')
+        print time.time(), 1, z[1]
+
     if key == Key.esc:
         return False
 
