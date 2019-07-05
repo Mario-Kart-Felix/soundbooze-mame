c224c0e8575caac5f4c771d61a46a72833abcf4f
soundbooze <soundbooze@gmail.com>
2019-07-05 13:14:57 +0700

    pre

--- sf2ce/ryu.py
+++ sf2ce/ryu.py
@@ -125,6 +125,48 @@ class RYU:
         os.system('xdotool key --window ' + self.winid + ' keyup ' + z)
         os.system('xdotool key --window ' + self.winid + ' keyup D')
 
+    def act(self, r):
+        if r == 0:
+          self.punch()
+        elif r == 1:
+          self.kick()
+        elif r == 2:
+          self.downkick()
+        elif r == 3:
+          self.kick()
+          self.right()
+          self.kick()
+        elif r == 4:
+          self.kick()
+          self.jumpup()
+          self.kick()
+        elif r == 5:
+          self.jumpleft(0.6)
+          self.kick()
+        elif r == 6:
+          self.jumpright(0.6)
+          self.kick()
+        elif r == 7:
+          self.fire(0)
+        elif r == 8:
+          self.fire(1)
+        elif r == 9:
+          self.superpunch(0)
+        elif r == 10:
+          self.superpunch(1)
+        elif r == 11:
+          self.superkick(0)
+        elif r == 12:
+          self.superkick(1)
+        elif r == 13:
+          self.defendup(0)
+        elif r == 14:
+          self.defendup(1)
+        elif r == 15:
+          self.defenddown(0)
+        elif r == 16:
+          self.defenddown(1)
+
     def insertcoin(self):
         os.system('xdotool key --window ' + self.winid + ' keydown 5')
         time.sleep(0.2)
