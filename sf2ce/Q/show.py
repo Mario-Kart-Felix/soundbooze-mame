7ce6b5ef018660d3536d2d0843656b50b313c027
soundbooze <soundbooze@gmail.com>
2019-07-07 00:56:00 +0700

    pre

--- sf2ce/Q/show.py
+++ sf2ce/Q/show.py
@@ -11,31 +11,42 @@ def show():
     print '[HQ]:', len(HQ)
 
     Q         = [] 
-    HP, HR    = [], []
+    R         = []
+    LP, LR    = [], []
     Pts, Rts  = [], []
 
     for k, v in HQ.items():
         Q.append(v[0])
+        R.append(v[2])
+
         p = v[1][0]
         r = v[1][1]
         if p == -1:
-            HP.append(p)
+            LP.append(p)
         if r == 1:
-            HR.append(r)
+            LR.append(r)
 
         Pts.append(p)
         Rts.append(r)
 
+    plt.title('Q')
+    plt.subplot(211)
     Q = numpy.array(Q)
     sns.heatmap(Q)
+
+    plt.title('R')
+    plt.subplot(212)
+    R = numpy.array(R)
+    sns.heatmap(R)
+
     plt.show()
 
     plt.subplot(211)
-    plt.title('[Penalty: ' + str(len(HP)) + ']')
+    plt.title('[Penalty: ' + str(len(LP)) + ']')
     plt.bar(range(len(Pts)), Pts)
 
     plt.subplot(212)
-    plt.title('[Reward: ' + str(len(HR)) + ']')
+    plt.title('[Reward: ' + str(len(LR)) + ']')
     plt.bar(range(len(Rts)), Rts)
 
     plt.show()
