d8132210e97a229304cd3cdfa3820f191600d6d6
soundbooze <soundbooze@gmail.com>
2019-07-10 01:03:05 +0700

    pre

--- sf2ce/obsolete/feed/bison.py
+++ sf2ce/obsolete/feed/bison.py
@@ -110,15 +110,15 @@ class BISON:
             z = self.r
 
         os.system('xdotool key --window ' + self.winid + ' keydown ' + z)
-        time.sleep(0.6)
+        time.sleep(0.3)
         os.system('xdotool key --window ' + self.winid + ' keydown ' + self.down)
         os.system('xdotool key --window ' + self.winid + ' keyup ' + z)
         os.system('xdotool key --window ' + self.winid + ' keydown ' + z)
         time.sleep(1.8)
         os.system('xdotool key --window ' + self.winid + ' keyup ' + self.down)
-        os.system('xdotool key --window ' + self.winid + ' keydown ' + self.up)
+        os.system('xdotool key --window ' + self.winid + ' key ' + z)
+        os.system('xdotool key --window ' + self.winid + ' key ' + self.up)
         os.system('xdotool key --window ' + self.winid + ' keydown ' + self.kk)
-        os.system('xdotool key --window ' + self.winid + ' keyup ' + z)
         os.system('xdotool key --window ' + self.winid + ' keyup ' + self.kk)
         '''
         time.sleep(0.4)
