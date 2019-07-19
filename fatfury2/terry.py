c3c2f878f7d5184fce9d92d62bd810013f34ef0e
soundbooze <soundbooze@gmail.com>
2019-07-19 13:41:32 +0700

    pre

--- fatfury2/terry.py
+++ fatfury2/terry.py
@@ -4,45 +4,51 @@ import os
 
 class TERRY:
 
-    def __init__(self):
+    def __init__(self, l, r, u, d, p, k):
         cmd = "xdotool search --pid `pgrep mame`"
         r =  subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout
         v = r.read()
         self.winid = hex(int(v.decode()))
         os.system('xdotool windowfocus --sync ' + self.winid)
+        self.l = l
+        self.r = r
+        self.u = u
+        self.d = d
+        self.p = p
+        self.k = k
 
     def punch(self):
-        os.system('xdotool key --window ' + self.winid + ' key D')
+        os.system('xdotool key --window ' + self.winid + ' key ' + self.p)
 
     def kick(self):
-        os.system('xdotool key --window ' + self.winid + ' key F')
+        os.system('xdotool key --window ' + self.winid + ' key ' + self.k)
 
     def downpunch(self):
-        os.system('xdotool key --window ' + self.winid + ' keydown Down')
-        os.system('xdotool key --window ' + self.winid + ' key D')
-        os.system('xdotool key --window ' + self.winid + ' keyup Down')
+        os.system('xdotool key --window ' + self.winid + ' keydown ' + self.d)
+        os.system('xdotool key --window ' + self.winid + ' key ' + self.p)
+        os.system('xdotool key --window ' + self.winid + ' keyup ' + self.d)
 
     def downkick(self):
-        os.system('xdotool key --window ' + self.winid + ' keydown Down')
-        os.system('xdotool key --window ' + self.winid + ' key F')
-        os.system('xdotool key --window ' + self.winid + ' keyup Down')
+        os.system('xdotool key --window ' + self.winid + ' keydown ' + self.d)
+        os.system('xdotool key --window ' + self.winid + ' key ' + self.k)
+        os.system('xdotool key --window ' + self.winid + ' keyup ' + self.d)
 
     def left(self, ts):
-        os.system('xdotool key --window ' + self.winid + ' keydown Left')
+        os.system('xdotool key --window ' + self.winid + ' keydown ' + self.l)
         time.sleep(ts)
-        os.system('xdotool key --window ' + self.winid + ' keyup Left')
+        os.system('xdotool key --window ' + self.winid + ' keyup ' + self.l)
 
     def right(self, ts):
-        os.system('xdotool key --window ' + self.winid + ' keydown Right')
+        os.system('xdotool key --window ' + self.winid + ' keydown ' + self.r)
         time.sleep(ts)
-        os.system('xdotool key --window ' + self.winid + ' keyup Right')
+        os.system('xdotool key --window ' + self.winid + ' keyup ' + self.r)
 
     def shift(self, pos):
-        key = "Left"
+        key = self.l
         if pos == 0:
-            key = "Left"
+            key = self.l
         elif pos == 1:
-            key = "Right"
+            key = self.r
 
         for i in range(4):
             os.system('xdotool key --window ' + self.winid + ' keydown ' + key)
@@ -50,103 +56,103 @@ class TERRY:
             os.system('xdotool key --window ' + self.winid + ' keyup ' + key)
 
     def powerwave(self, pos):
-        z = "Right"
+        z = self.r
         if pos == 0:
-            z = "Right"
+            z = self.r
         else:
-            z = "Left"
+            z = self.l
 
-        os.system('xdotool key --window ' + self.winid + ' keydown Down')
+        os.system('xdotool key --window ' + self.winid + ' keydown ' + self.d)
         os.system('xdotool key --window ' + self.winid + ' keydown ' + z)
-        os.system('xdotool key --window ' + self.winid + ' keydown D')
-        os.system('xdotool key --window ' + self.winid + ' keyup Down')
+        os.system('xdotool key --window ' + self.winid + ' keydown ' + self.p)
+        os.system('xdotool key --window ' + self.winid + ' keyup ' + self.d)
         os.system('xdotool key --window ' + self.winid + ' keyup ' + z)
-        os.system('xdotool key --window ' + self.winid + ' keyup D')
+        os.system('xdotool key --window ' + self.winid + ' keyup ' + self.p)
 
     def burnknuckle(self, pos):
-        z = "Left"
+        z = self.l
         if pos == 0:
-            z = "Left"
+            z = self.l
         else:
-            z = "Right"
+            z = self.r
 
-        os.system('xdotool key --window ' + self.winid + ' keydown Down')
+        os.system('xdotool key --window ' + self.winid + ' keydown ' + self.d)
         os.system('xdotool key --window ' + self.winid + ' keydown ' + z)
-        os.system('xdotool key --window ' + self.winid + ' keydown D')
-        os.system('xdotool key --window ' + self.winid + ' keyup Down')
+        os.system('xdotool key --window ' + self.winid + ' keydown ' + self.p)
+        os.system('xdotool key --window ' + self.winid + ' keyup ' + self.d)
         os.system('xdotool key --window ' + self.winid + ' keyup ' + z)
-        os.system('xdotool key --window ' + self.winid + ' keyup D')
+        os.system('xdotool key --window ' + self.winid + ' keyup ' + self.p)
 
     def risingtackle(self, pos):
-        z = "Left"
+        z = self.l
         if pos == 0:
-            z = "Left"
+            z = self.l
         else:
-            z = "Right"
+            z = self.r
         for i in range(0,42):
-            os.system('xdotool key --window ' + self.winid + ' keydown ' + z + ' Down')
-        os.system('xdotool key --window ' + self.winid + ' keydown Up')
-        os.system('xdotool key --window ' + self.winid + ' keydown D')
+            os.system('xdotool key --window ' + self.winid + ' keydown ' + z + ' ' + self.d)
+        os.system('xdotool key --window ' + self.winid + ' keydown ' + self.u)
+        os.system('xdotool key --window ' + self.winid + ' keydown ' + self.p)
         for i in range(0,42):
-            os.system('xdotool key --window ' + self.winid + ' keyup ' + z + ' Down')
-        os.system('xdotool key --window ' + self.winid + ' keyup Up')
-        os.system('xdotool key --window ' + self.winid + ' keyup D')
+            os.system('xdotool key --window ' + self.winid + ' keyup ' + z + ' ' + self.d)
+        os.system('xdotool key --window ' + self.winid + ' keyup ' + self.u)
+        os.system('xdotool key --window ' + self.winid + ' keyup ' + self.p)
 
     def crackshoot(self, pos):
-        z = "Left"
+        z = self.l
         if pos == 0:
-            z = "Left"
+            z = self.l
         else:
-            z = "Right"
+            z = self.r
 
         for i in range(0,6):
-            os.system('xdotool key --window ' + self.winid + ' keydown Down')
+            os.system('xdotool key --window ' + self.winid + ' keydown ' + self.d)
         os.system('xdotool key --window ' + self.winid + ' keydown ' + z)
-        os.system('xdotool key --window ' + self.winid + ' keydown Up')
-        os.system('xdotool key --window ' + self.winid + ' keydown F')
+        os.system('xdotool key --window ' + self.winid + ' keydown ' + self.u)
+        os.system('xdotool key --window ' + self.winid + ' keydown ' + self.k)
         for i in range(0,6):
-            os.system('xdotool key --window ' + self.winid + ' keyup Down')
+            os.system('xdotool key --window ' + self.winid + ' keyup ' + self.d)
         os.system('xdotool key --window ' + self.winid + ' keyup ' + z)
-        os.system('xdotool key --window ' + self.winid + ' keyup Up')
-        os.system('xdotool key --window ' + self.winid + ' keyup F')
+        os.system('xdotool key --window ' + self.winid + ' keyup ' + self.u)
+        os.system('xdotool key --window ' + self.winid + ' keyup ' + self.k)
 
     def defendup(self, pos, ts):
-        key = "Left"
+        key = self.l
         if pos == 0:
-            key = "Left"
+            key = self.l
         elif pos == 1:
-            key = "Right"
+            key = self.r
 
         os.system('xdotool key --window ' + self.winid + ' keydown ' + key)
         time.sleep(ts)
         os.system('xdotool key --window ' + self.winid + ' keyup ' + key)
 
     def defenddown(self, pos, ts):
-        key = "Left"
+        key = self.l
         if pos == 0:
-            key = "Left"
+            key = self.l
         elif pos == 1:
-            key = "Right"
+            key = self.r
 
         os.system('xdotool key --window ' + self.winid + ' keydown ' + key)
-        os.system('xdotool key --window ' + self.winid + ' keydown Down')
+        os.system('xdotool key --window ' + self.winid + ' keydown ' + self.d)
         time.sleep(ts)
         os.system('xdotool key --window ' + self.winid + ' keyup ' + key)
         os.system('xdotool key --window ' + self.winid + ' keyup Down')
 
     def jumpright(self, ts):
-        os.system('xdotool key --window ' + self.winid + ' keydown Right')
-        os.system('xdotool key --window ' + self.winid + ' keydown Up')
+        os.system('xdotool key --window ' + self.winid + ' keydown ' + self.r)
+        os.system('xdotool key --window ' + self.winid + ' keydown ' + self.u)
         time.sleep(ts)
-        os.system('xdotool key --window ' + self.winid + ' keyup Right')
-        os.system('xdotool key --window ' + self.winid + ' keyup Up')
+        os.system('xdotool key --window ' + self.winid + ' keyup ' + self.r)
+        os.system('xdotool key --window ' + self.winid + ' keyup ' + self.u)
 
     def jumpleft(self, ts):
-        os.system('xdotool key --window ' + self.winid + ' keydown Left')
-        os.system('xdotool key --window ' + self.winid + ' keydown Up')
+        os.system('xdotool key --window ' + self.winid + ' keydown ' + self.l)
+        os.system('xdotool key --window ' + self.winid + ' keydown ' + self.u)
         time.sleep(ts)
-        os.system('xdotool key --window ' + self.winid + ' keyup Left')
-        os.system('xdotool key --window ' + self.winid + ' keyup Up')
+        os.system('xdotool key --window ' + self.winid + ' keyup ' + self.l)
+        os.system('xdotool key --window ' + self.winid + ' keyup ' + self.u)
 
     def insertcoin(self):
         time.sleep(1)
