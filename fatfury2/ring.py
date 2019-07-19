ef476fc2bc8b51b0880f670bf9569f24478101ee
soundbooze <soundbooze@gmail.com>
2019-07-19 12:34:55 +0700

    pre

--- fatfury2/ring.py
+++ fatfury2/ring.py
@@ -2,7 +2,7 @@ class RINGBUFFER:
 
     def __init__(self, size):
         self.data = [None for i in xrange(size)]
-        for i in range(siz):
+        for i in range(size):
             self.append('')
 
     def append(self, x):
