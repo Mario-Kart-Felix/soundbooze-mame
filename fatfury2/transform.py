2e6476d9190446c5a85ec5b8e433b4db070ea5a1
soundbooze <soundbooze@gmail.com>
2019-07-19 11:56:30 +0700

    pre

--- fatfury2/transform.py
+++ fatfury2/transform.py
@@ -1,22 +1,13 @@
+import cv2
+
 class TRANSFORM:
 
-    def red(self, frame):
-        r = frame.copy()
-        r[:,:,0] = 0
-        r[:,:,1] = 0
-        r[r < 250] = 0
-        return r
-
-    def green(self, frame):
-        g = frame.copy()
-        g[:,:,0] = 0
-        g[:,:,2] = 0
-        g[g < 250] = 0
-        return g
+    def __init__(self):
+        self.size = (400, 200)
 
     def blue(self, frame):
         b = frame.copy()
         b[:,:,1] = 0
         b[:,:,2] = 0
         b[b < 250] = 0
-        return b
+        return cv2.resize(b, self.size)
