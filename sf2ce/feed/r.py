fcfca39b2ad3342f1d75fa0a9a4cab3e71d8e905
soundbooze <soundbooze@gmail.com>
2019-08-09 09:36:24 +0700

    pre

--- sf2ce/feed/r.py
+++ sf2ce/feed/r.py
@@ -8,34 +8,52 @@ from ryu import *
 BLOOD      = [2744512, 4089536, 745816 * 4]
 RESUME     = [1358640, 2617406, 2264400, 2623509]
 
-def act(r):
+def act(hi, mid, lo, r):
 
     if r == 0:
-        ryu.fire(numpy.random.randint(0,2))
+        z = numpy.random.randint(0,3)
+        if z == 0:
+            lo.fire(numpy.random.randint(0,2))
+        elif z == 1:
+            mid.fire(numpy.random.randint(0,2))
+        elif z == 2:
+            hi.fire(numpy.random.randint(0,2))
     elif r == 1:
-        ryu.superpunch(numpy.random.randint(0,2))
+        z = numpy.random.randint(0,3)
+        if z == 0:
+            lo.superpunch(numpy.random.randint(0,2))
+        elif z == 1:
+            mid.superpunch(numpy.random.randint(0,2))
+        elif z == 2:
+            hi.superpunch(numpy.random.randint(0,2))
     elif r == 2:
-        ryu.superkick(numpy.random.randint(0,2))
+        z = numpy.random.randint(0,3)
+        if z == 0:
+            lo.superkick(numpy.random.randint(0,2))
+        elif z == 2:
+            mid.superkick(numpy.random.randint(0,2))
+        elif z == 3:
+            hi.superkick(numpy.random.randint(0,2))
     elif r == 3:
         z = numpy.random.randint(0,4)
         if z == 0:
-            ryu.downkick()
+            hi.downkick()
         elif z == 1:
-            ryu.kick()
-            ryu.jumpup()
-            ryu.kick()
+            hi.kick()
+            hi.jumpup()
+            hi.kick()
         elif z == 2:
-            ryu.jumpright(0.3)
-            ryu.kick()
+            hi.jumpright(numpy.random.uniform(low=0.3, high=0.5))
+            hi.kick()
         elif z == 3:
-            ryu.jumpleft(0.3)
-            ryu.kick()
+            hi.jumpleft(numpy.random.uniform(low=0.3, high=0.5))
+            hi.kick()
     elif r == 4:
         z = numpy.random.randint(0,2)
         if z == 0:
-            ryu.defendup(numpy.random.randint(0,2))
+            hi.defendup(numpy.random.randint(0,2))
         elif z == 0:
-            ryu.defenddown(numpy.random.randint(0,2))
+            hi.defenddown(numpy.random.randint(0,2))
 
 with mss.mss() as sct:
 
@@ -44,7 +62,9 @@ with mss.mss() as sct:
 
     play = False
 
-    ryu = RYU('Left', 'Right', 'Up', 'Down', 'c', 'd')
+    hi  = RYU('Left', 'Right', 'Up', 'Down', 'c', 'd')
+    mid = RYU('Left', 'Right', 'Up', 'Down', 'x', 's')
+    lo  = RYU('Left', 'Right', 'Up', 'Down', 'z', 'a')
 
     while [ 1 ]:
 
@@ -66,8 +86,8 @@ with mss.mss() as sct:
         if sumb1 >= BLOOD[0] and sumb1 <= BLOOD[1]:
 
             if play:
-                act(numpy.random.randint(0,5))
-                time.sleep(0.11)
+                act(hi, mid, lo, numpy.random.randint(0,5))
+                time.sleep(numpy.random.uniform(low=0.05, high=0.2))
                 
             if sumb1 == BLOOD[1] and sumb2 == BLOOD[1] and not play:
                 print '[Start]'
@@ -76,18 +96,16 @@ with mss.mss() as sct:
 
             elif sumb1 == BLOOD[0] and rbsum == BLOOD[2]:
                 print 'P1 [KO]'
-                hash.flush()
                 play = False
                 time.sleep(1)
 
             elif sumb2 == BLOOD[0] and rbsum == BLOOD[2]:
                 print 'P2 [KO]'
-                hash.flush()
                 play = False
                 time.sleep(1)
 
         elif sumb1 == RESUME[0]:
-            ryu.insertcoin()
+            hi.insertcoin()
         
         elif sumb1 == RESUME[1] or sumb1 == RESUME[2] or sumb1 == RESUME[3]:
-            ryu.select()
+            hi.select()
