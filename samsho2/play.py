5d31135b4691bbd044ec51a2ac4f220f7c140609
soundbooze <soundbooze@gmail.com>
2019-08-09 12:58:54 +0700

    pre

--- samsho2/play.py
+++ samsho2/play.py
@@ -7,11 +7,13 @@ if __name__ == '__main__':
 
     with mss.mss() as sct:
 
+        action = ['Walk(0)','Shift(0)','DefendUp(0)','DefendDown(0)','StabSwingBack(0)','OugiSenpuuRetsuZan(0)','OugiKogetsuZan(0)','OugiResshinZan(0)','JumpLeftSlash()','JumpSlash()','DownSlash()','Slash()','Walk(1)','Shift(1)','DefendUp(1)','DefendDown(1)','StabSwingBack(1)','OugiSenpuuRetsuZan(1)','OugiKogetsuZan(1)','OugiResshinZan(1)','JumpRightSlash()']
         haohmaru  = HAOHMARU('Left', 'Right', 'Up', 'Down', 's', 'z')
 
         while [ 1 ]:
 
             r = numpy.random.randint(0, 21)
+            print action[r]
             haohmaru.act(r)
 
     
