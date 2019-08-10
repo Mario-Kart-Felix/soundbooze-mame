9b4766ab9ed6897809c19a8636e66db9d4602f30
soundbooze <soundbooze@gmail.com>
2019-08-10 18:37:42 +0700

    pre

---
+++ samsho2/scene.py
@@ -0,0 +1,6 @@
+class SCENE:
+
+    def __init__(self):
+        self.full = {"top": 124, "left": 100, "width": 800, "height": 600}
+        self.head = {"top": 124, "left": 100, "width": 800, "height": 100}
+        self.body = {"top": 264, "left": 100, "width": 800, "height": 400}
