772b769d7b438df4750c182225d769fb8b05b352
soundbooze <soundbooze@gmail.com>
2019-08-10 23:02:23 +0700

    pre

--- samsho2/similar.py
+++ samsho2/similar.py
@@ -4,7 +4,7 @@ from skimage.measure import compare_ssim
 
 class SIMILAR:
 
-    def compute(img_a, img_b):
+    def compute(self, img_a, img_b):
         img_a = cv2.cvtColor(img_a, cv2.COLOR_BGR2GRAY)
         img_b = cv2.cvtColor(img_b, cv2.COLOR_BGR2GRAY)
         sim, _ = compare_ssim(numpy.array(img_a), numpy.array(img_b), full=True)
