90e66139a8b865dc86cbf01e7522a7ebcf1fc11d
soundbooze <soundbooze@gmail.com>
2019-08-10 18:44:37 +0700

    pre

---
+++ samsho2/act.py
@@ -0,0 +1,9 @@
+import numpy
+
+class ACT:
+
+    def __init__(self):
+        self.action = ['Walk(0)','Shift(0)','DefendUp(0)','DefendDown(0)','StabSwingBack(0)','OugiSenpuuRetsuZan(0)','OugiKogetsuZan(0)','OugiResshinZan(0)','TenhaSeiouZan(0)','Hide(0)','JumpLeftSlash()','JumpSlash()','DownSlash()','Slash()','Walk(1)','Shift(1)','DefendUp(1)','DefendDown(1)','StabSwingBack(1)','OugiSenpuuRetsuZan(1)','OugiKogetsuZan(1)','OugiResshinZan(1)','TenhaSeiouZan(1)','Hide(1)','JumpRightSlash()']
+
+    def next(self):
+        return numpy.random.randint(0, len(self.action))
