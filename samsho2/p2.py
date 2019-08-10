98f26341166a7980f456d56073c7c9f21a646e4d
soundbooze <soundbooze@gmail.com>
2019-08-10 22:14:33 +0700

    pre

--- samsho2/p2.py
+++ samsho2/p2.py
@@ -20,8 +20,15 @@ if __name__ == '__main__':
 
         while [ 1 ]:
 
+            start_ts = time.time()
+
             full  = numpy.array(sct.grab(scene.full))
             frame = numpy.array(sct.grab(scene.body))
             r = act.next()
             act.act(r)
             print phash.compute(frame), '-', act.action[r], numpy.sum(hist.compute(full)), numpy.sum(dft.compute(full))/1000000.0, numpy.sum(dft.threshold(full)/100.0)
+
+            fps = 1 / (time.time() - start_ts)
+            delta = 1 / fps - (time.time() - start_ts)
+            if delta > 0:
+                time.sleep(delta)
