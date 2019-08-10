658e93b95c4b088364cc06ace7db99ff827e6ee3
soundbooze <soundbooze@gmail.com>
2019-08-10 23:30:15 +0700

    pre

---
+++ samsho2/template.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy
+
+class TEMPLATE:
+
+    def __init__(self):
+        self.threshold = 0.9
+
+    def blood(self):
+        return cv2.imread('template/blood.png', 0)
+ 
+    def match(self, background, template):
+        gray = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
+        result = cv2.matchTemplate(gray,template,cv2.TM_CCOEFF_NORMED)
+        loc = numpy.where(result >= self.threshold)
+        (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
+        if len(loc[0]) > 1:
+            return True
+        else:
+            return False
